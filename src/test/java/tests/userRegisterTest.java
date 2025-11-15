1b164810609fa2cb7877bcc2ffc122a0d1d249b9
Irina Lebed <franlyaguh@gmail.com>
2025-11-15 13:40:56 +0300

    after videos

---
+++ src/test/java/tests/userRegisterTest.java
@@ -0,0 +1,47 @@
+package tests;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import lib.Assertions;
+import lib.BaseTestCase;
+import lib.DataGenerator;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class userRegisterTest extends BaseTestCase {
+    @Test
+    public void testCreateUserWithExistingEmail() {
+        Map<String,String> userData = new HashMap<>();
+        String email = "vinkotov@example.com";
+        userData.put("email",email);
+        userData = DataGenerator.getRegistrationData(userData);
+
+        Response responseCreateAuth = RestAssured
+                .given()
+                .body(userData)
+                .post("https://playground.learnqa.ru/api/user/")
+                .andReturn();
+
+        Assertions.assertResponseCodeEquals(responseCreateAuth,400);
+        Assertions.assertResponseTextEquals(responseCreateAuth,"Users with email '"+email+"' already exists");
+            }
+
+    @Test
+    public void testCreateUserSuccessfilly() {
+        String email = DataGenerator.getRandomEmail();
+
+        Map<String,String> userData = DataGenerator.getRegistrationData();
+
+        Response responseCreateAuth = RestAssured
+                .given()
+                .body(userData)
+                .post("https://playground.learnqa.ru/api/user/")
+                .andReturn();
+
+        Assertions.assertResponseCodeEquals(responseCreateAuth,200);
+        Assertions.assertJsonHasField(responseCreateAuth,"id");
+    }
+
+}
